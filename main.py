bbdec6d081b13437a5bb05b23729c69b7cff0591
hippolyte-dubois <hippolyte.dubois@etu.unilim.fr>
2018-03-05 22:17:19 +0100

    changed dictionnary and rules

--- main.py
+++ main.py
@@ -3,52 +3,94 @@
 from random import choice, randint
 import datetime, sys, textwrap
 from time import gmtime, strftime
+import argparse
 
-nouns = open('nouns.txt').read().split()
-adjectives = open('adjectives.txt').read().split()
-verbs = open('verbs.txt').read().split()
+def num(s):
+	try:
+		return int(s)
+	except ValueError:
+		return float(s)
 
-author ="kptn"
-
-def header():
+def header(n, author):
 	print("*" * 40)
-	print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-	print("=" * (9 - int(len(author) / 2) + len(author) % 2) +" a poem written by ~" + author + "~ " + "=" * (9 - int(len(author) / 2)))
+	print("* {:5} *****".format(n) + strftime(" %c *", gmtime()))
+	print("*" * (40 - len(author) - 6) + " by " + author + " *")
 	print("*" * 40)
 	print()
 
-def poem(min,max):
-	for x in range(randint(min,max)):
+def poem(length, nouns, adjectives, verbs):
+	for x in range(length):
 		words1 = " ".join([choice(adjectives) + ", ", choice(adjectives)])
 		words2 = " ".join([choice(nouns), choice(verbs)])
 		words3 = " ".join([choice(nouns), choice(nouns), choice(adjectives), choice(nouns)])
 
 		for i in range(randint(2,5)):
 			words = choice([words1, words2, words3])
-			line = " "*randint(0, 40 - len(words)) + words
+			line = ""
+			if len(words) < 40:
+				line = " "*randint(0, 40 - len(words)) + words
+			else:
+	    			line = words
 			print(line)
-        
+
 		if x % 3 ==0:
 			print()
-			print(" "*5 + choice(nouns))
-			print(" "*5 + choice(nouns))
+			print(" "*5 + choice(adjectives))
+			print(" "*5 + choice(verbs))
 			print(" "*5 + "the " + choice(nouns) +"!")
 			print()
-	        
+
+		if x % 5 ==0:
+			print()
+			print(" "*1 + choice(nouns) + "y")
+			verb = choice(verbs)
+			if verb[-1] == "e":
+    				print(" "*3 + choice(verbs)[:-1] + "ing")
+			else:
+					print(" "*3 + choice(verbs) + "ing")
+			n = choice(nouns)
+			if n[-1] == "e":
+				n = n + "r"
+			if n[-3:] == "ion":
+				n = n[:-3] + "or"
+			else:
+				n = n + "er"
+			print(" "*5 + "the" + ((" " + n) * randint(1,3)) +"!")
+			print()
+
 		if x % 7 ==0:
-			words4 = " ".join(["the " + choice(adjectives), choice(nouns), choice(verbs)])
 			print()
-			s = (" ").join(words4)
-			print(textwrap.fill(s , width=41))
+			print("the " + choice(adjectives) + " " + choice(nouns))
+			print(choice(verbs)+ "s")
+			print("the " + choice(adjectives) + " " + choice(nouns))
 			print()
 
+		if x % 11 ==0:
+			print("~to " + choice(verbs) + " is to " + choice(verbs))
+			print()
 
-old_stdout = sys.stdout
-log_file = open("poems.txt","a")
-sys.stdout = log_file
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("author", type=str, help="the author\'s name")
+	parser.add_argument("nouns", type=str, help="the file containing the noun database. Each word must be on his line")
+	parser.add_argument("adjectives", type=str, help="the file containing the adjectives database. Each word must be on his line")
+	parser.add_argument("verbs", type=str, help="the file containing the verbs database. Each word must be on his line")
+	parser.add_argument("output", type=str, help="the output file")
+	args = parser.parse_args()
 
-header()
-poem(5,11)
+	nouns = open(args.nouns).read().split('\n')
+	adjectives = open(args.adjectives).read().split('\n')
+	verbs = open(args.verbs).read().split('\n')
 
-sys.stdout = old_stdout
-log_file.close()
+	old_stdout = sys.stdout
+	log_file = open(args.output,"a")
+	sys.stdout = log_file
+
+	length = 150
+
+	for n in range(1,length):
+		header(n, args.author)
+		poem(3, nouns, adjectives, verbs)
+
+	sys.stdout = old_stdout
+	log_file.close()
